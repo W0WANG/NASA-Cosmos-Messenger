83de062c9b117cbc11484eeae94acc1c784ca344
WW0 <C110118117@nkust.edu.tw>
2026-04-17 23:12:18 +0800

    update targetSdkVersion

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.nasacosmosmessenger"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.nasacosmosmessenger"
